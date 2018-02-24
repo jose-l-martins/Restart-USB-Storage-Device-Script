d8f4b7bc45d5165ed73fcb0f6fa17ec77f62b461
jose-l-martins <jose-luis-martins@outlook.com>
2018-02-24 15:37:08 +0000

    Reformat comments

--- Restart_usb_ext_hdd_device.ps1
+++ Restart_usb_ext_hdd_device.ps1
@@ -1,5 +1,12 @@
-# I wrote this script was developed to restart my Toshiba external Canvio Desk 2TB 3.5'' hard drive device with faulty connection to a Windows 10 OS. It's unknown whether the problem is hardware-related (faulty/damaged data or power cables) or software-related, although, perhaps it's the later, since the problem doesn't seem to manifest, when trying it out on Linux OS distros.
-# Use the "pnputil.exe /enum-devices" to find all devices available in the computer. Filter the output (e.g. copying it to a text editor and looking up the name of the drive formated in the USB storage device via CTRL+F, e.g. "Toshiba").
+<#
+This script restarts a Toshiba external Canvio Desk 2TB 3.5'' hard drive device with a faulty connection to a Windows 10 OS. 
+The problem could be hardware-related (faulty/damaged data or power cables) or software-related. 
+The issue does not seem to manifest on Linux OS distros, suggesting it might be software-related.
+
+Steps to find all devices available in the computer:
+1. Use the "pnputil.exe /enum-devices" command.
+2. Filter the output by copying it to a text editor and looking up the name of the drive formatted in the USB storage device (e.g., "Toshiba").
+#>
 
 $deviceInstanceId = "USB\VID_054C&PID_C005\5&276264ac&0&11"  # Replace with the actual Device Instance ID
 
